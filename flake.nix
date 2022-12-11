ddcf09793f00acceb03396aad45c39e77622886b
Loïc Reynier <loic@loicreynier.fr>
2022-12-11 22:26:36 +0100

    build(pre-commit): update hooks & add typos

--- flake.nix
+++ flake.nix
@@ -29,21 +29,15 @@
       checks = {
         pre-commit-check = pre-commit-hooks.lib.${system}.run {
           src = ./.;
+
           hooks = with pkgs; {
             alejandra.enable = true;
-            commitizen = {
-              enable = true;
-              entry = "${pkgs.commitizen}/bin/cz check --commit-msg-file";
-              stages = ["commit-msg"];
-            };
-            editorconfig-checker = {
-              enable = true;
-              entry = "${pkgs.editorconfig-checker}/bin/editorconfig-checker";
-              types = ["file"];
-            };
+            commitizen.enable = true;
+            editorconfig-checker.enable = true;
             prettier.enable = true;
             statix.enable = true;
             stylua.enable = true;
+            typos.enable = true;
           };
         };
       };
